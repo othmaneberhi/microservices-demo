2fd71c6570b0f5a234eeb73b4455cf8273226179
Othmane berhi <omaxnoti@gmail.com>
2023-07-15 19:47:24 +0000

    created Customer repository

--- customer/src/main/java/org/example/service/CustomerService.java
+++ customer/src/main/java/org/example/service/CustomerService.java
@@ -2,15 +2,18 @@ package org.example.service;
 
 import org.example.model.Customer;
 import org.example.model.CustomerRegistrationRequest;
+import org.example.repository.ICustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(ICustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = Customer.builder()
                 .firstname(request.firstname())
                 .lastname(request.lastname())
                 .email(request.email())
                 .build();
+
+        customerRepository.save(customer);
     }
 }

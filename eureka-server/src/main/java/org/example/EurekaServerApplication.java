f7a22e107b327d3c66a1c881e38b6d96b1d92b6b
Othmane berhi <omaxnoti@gmail.com>
2023-07-18 16:46:00 +0000

    Setting up eureka server

---
+++ eureka-server/src/main/java/org/example/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EurekaServerApplication.class,args);
+        System.out.println( "Eureka server is running ..." );
+    }
+}

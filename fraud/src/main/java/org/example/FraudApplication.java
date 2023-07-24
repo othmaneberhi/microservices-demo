04d493b18947730094b801f39a7dda37fde8909e
Othmane berhi <omaxnoti@gmail.com>
2023-07-24 11:04:06 +0000

    Changed the discovery service to Eureka

--- fraud/src/main/java/org/example/FraudApplication.java
+++ fraud/src/main/java/org/example/FraudApplication.java
@@ -2,13 +2,13 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  *
  */
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class FraudApplication
 {
